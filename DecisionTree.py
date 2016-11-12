0200aa5fccdac8e80c2e48cbdc1068b4396324fc
Jonathan Arfa <jonathanarfa@gmail.com>
2016-11-12 13:36:56 -0500

    added --min_samples_leaf, refactored some code

--- DecisionTree.py
+++ DecisionTree.py
@@ -10,66 +10,25 @@ def find_potential_splits(data, p=0.05):
         (c, np.unique(splits[:, c])) for c in range(splits.shape[1])
     )
 
-def find_split(data, targets, potential_splits, leaf_only=False, max_features=None):
-    best_split = {
-        "N": len(targets),
-        "mean_predict": np.mean(targets),
-        "subtree_loss": np.inf, 
-        "split_feature": None,
-        "split_value": None, 
-        "children": [None, None],
-    }
-    if leaf_only:
-        best_split["subtree_loss"] = logloss(
-            targets,
-            best_split["mean_predict"] * np.ones_like(targets)
-        )
-        return best_split
-
-    if max_features is None:
-        max_features = len(potential_splits)
-
-    # Even when max_features=None, I'm randomizing the order by which splits are
-    # considered because otherwise I'd have to worry about breaking ties between
-    # 2 splits that lead to equal loss. This change only marginally slows down the code.
-    feature_indices = np.random.choice(
-        len(potential_splits), max_features, replace=False)
-
-    for i in feature_indices:
-        for s in np.random.permutation(potential_splits[i]):
-            split_ix = (data[:,i] >= s)
-            if sum(split_ix) in (0, len(split_ix)):
-                # i.e. if splitting the data here is not a real split
-                continue
-            predictions = np.zeros_like(targets)
-            predictions[split_ix] = np.mean(targets[split_ix])
-            predictions[-split_ix] = np.mean(targets[-split_ix])
-            ll = logloss(targets, predictions)
-            if ll < best_split["subtree_loss"]:
-                best_split["subtree_loss"] = ll
-                best_split["split_feature"] = i
-                best_split["split_value"] = s
-
-    if best_split["subtree_loss"] == np.inf:
-        # i.e. if no split we considered actually separates the data
-        best_split["subtree_loss"] = logloss(targets,
-            best_split["mean_predict"] * np.ones_like(targets))
-
-    return best_split
-
+# I'm borrowing some of the variable names from sklearn's implementation
 class DecisionTree():
-    # I'm borrowing some of the variable names from sklearn's implementation
-    def __init__(self, min_samples_split=5, loss="logloss", max_depth=None,
-        max_features=None):
-        # self.min_samples_leaf = min_samples_leaf # TODO
-        self.min_samples_split = min_samples_split
+    def __init__(
+        self,
+        loss="logloss",
+        min_samples_leaf=10,
+        min_samples_split=20,
+        max_depth=None,
+        max_features=None,
+        ):
+        self.min_samples_leaf = min_samples_leaf
+        self.min_samples_split = max(min_samples_split, min_samples_leaf)
         if loss != "logloss":
             raise NotImplementedError("Need to make more loss functions")
         self.loss = loss
-        self.potential_splits = None
-        self.tree = None
         self.max_depth = max_depth
         self.max_features = max_features
+        self.potential_splits = None
+        self.tree = None
 
     def _predict_event(self, event):
         subtree = self.tree
@@ -89,15 +48,59 @@ class DecisionTree():
             return self._predict_event(data)
         return np.apply_along_axis(self._predict_event, 1, data)
 
+    def find_split(self, data, targets, leaf_only=False):
+        mean_predict = np.mean(targets)
+        best_split = {
+            "N": len(targets),
+            "mean_predict": mean_predict,
+            # if no split can beat logloss(baserate_predictor), don't consider it
+            "subtree_loss": logloss(targets, mean_predict * np.ones_like(targets)),
+            "split_feature": None,
+            "split_value": None,
+            "children": [None, None],
+        }
+        if leaf_only:
+            return best_split
+
+        # Even when max_features=None, I'm randomizing the order by which splits are
+        # considered because otherwise I'd have to find a principled way to break ties
+        # between 2 splits that lead to equal loss. This change only marginally slows
+        # down the code.
+        max_features = self.max_features or len(self.potential_splits)
+        feature_indices = np.random.choice(len(self.potential_splits), max_features,
+                            replace=False)
+        # TODO: when multiple separations have equal loss, it would be nice to
+        # choose the one in the middle. Do other implementations do this?
+        for i in feature_indices:
+            for s in np.random.permutation(self.potential_splits[i]):
+                split_ix = data[:,i] >= s
+                if sum(split_ix) in (0, len(split_ix)):
+                    # i.e. if splitting the data here is not a real split
+                    continue
+                predictions = np.zeros_like(targets)
+                predictions[split_ix] = np.mean(targets[split_ix])
+                predictions[-split_ix] = np.mean(targets[-split_ix])
+                ll = logloss(targets, predictions)
+                if ll < best_split["subtree_loss"]:
+                    best_split["subtree_loss"] = ll
+                    best_split["split_feature"] = i
+                    best_split["split_value"] = s
+
+        return best_split
+
     def build_subtree(self, data, targets, depth):
         if (self.max_depth is not None and depth >= self.max_depth
             ) or data.shape[0] <= self.min_samples_split:
-            return find_split(data, targets, None, leaf_only=True)
+            return self.find_split(data, targets, leaf_only=True)
 
-        split = find_split(data, targets, self.potential_splits, max_features=self.max_features)
+        split = self.find_split(data, targets, leaf_only=False)
+        if split["split_feature"] is None:
+            return split
         split_indices = data[:,split["split_feature"]] >= split["split_value"]
-        split["children"][0] = self.build_subtree(data[-split_indices, :], targets[-split_indices], depth=depth+1)
-        split["children"][1] = self.build_subtree(data[split_indices, :], targets[split_indices], depth=depth+1)
+        if sum(-split_indices) >= self.min_samples_leaf:
+            split["children"][0] = self.build_subtree(data[-split_indices, :], targets[-split_indices], depth=depth+1)
+        if sum(split_indices) >= self.min_samples_leaf:
+            split["children"][1] = self.build_subtree(data[split_indices, :], targets[split_indices], depth=depth+1)
         return split
 
     def train(self, data, targets):
@@ -120,7 +123,6 @@ class DecisionTree():
             indent=4, sort_keys=True)
 
 
-
 if __name__ == "__main__":
     import argparse
     from sklearn import datasets
@@ -135,6 +137,7 @@ if __name__ == "__main__":
                         help='holdout proportion (0, 1.0)')
     parser.add_argument('--max_depth', type=int, default=None)
     parser.add_argument('--min_samples_split', type=int, default=20)
+    parser.add_argument('--min_samples_leaf', type=int, default=10)
     parser.add_argument("--max_features", type=int, default=None)
     args = parser.parse_args()
 
@@ -148,6 +151,7 @@ if __name__ == "__main__":
     )
 
     tree = DecisionTree(
+        min_samples_leaf=args.min_samples_leaf,
         min_samples_split=args.min_samples_split,
         max_depth=args.max_depth,
         max_features=args.max_features)
